0cb529eee977045f423b0dc2b09b28aeeebb4e51
samar gaiche <samargaiche82@gmail.com>
2022-09-14 21:58:42 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+let themeColor = document.querySelectorAll('.theme-toggler span');
+themeColor.forEach(color => color.addEventListener('click', () =>{
+    let background = color.style.background;
+    document.querySelector('body').style.background = background;
+}));
